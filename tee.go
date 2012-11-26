dc564079520e00ad4749f36ef2c30edeb02faea4
Eduardo Arino de la Rubia <earino@gmail.com>
2012-11-25 18:13:22 -0800

    tee is now working better :-) removed all buffering & traps interrupts

--- tee.go
+++ tee.go
@@ -2,48 +2,44 @@ package main
 
 import (
 	"io"
-	"bufio"
 	"os"
+	"os/signal"
 	"flag"
 	"log"
 )
 
 var appendPtr = flag.Bool("a", false, "Append the output to the files rather than overwriting them.")
-var ignoreCasePtr = flag.Bool("i", false, "Ignore the SIGINT signal.")
+var ignoreIntPtr = flag.Bool("i", false, "Ignore the SIGINT signal.")
 
 func main() {
 	flag.Parse()
 
 	var writers = []io.Writer{os.Stdout}
+	var fileFlags int = os.O_WRONLY|os.O_CREATE
+
+	if *appendPtr {
+		fileFlags |= os.O_APPEND
+	} else {
+		fileFlags |= os.O_TRUNC
+	}
+
+	if *ignoreIntPtr {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, os.Interrupt)
+	}
 
 	for _, teeTo := range flag.Args() {
-		var fileFlags int
-
-		if *appendPtr {
-			fileFlags = os.O_WRONLY|os.O_CREATE|os.O_APPEND
-		} else {
-			fileFlags = os.O_WRONLY|os.O_CREATE|os.O_TRUNC
-		}
-
 		w, err := os.OpenFile(teeTo, fileFlags, 0644)
 
 		if err != nil {
 			log.Fatal(err)
 		}
 		
-		writer := io.Writer(w)
-		bufwriter := bufio.NewWriter(writer)
-
-		defer w.Close()
-		defer bufwriter.Flush()
-
-		writers = append(writers, bufwriter)
+		writers = append(writers, w)
 	}
 
 	teeWriter := io.MultiWriter(writers...)
-	bufwriter := bufio.NewWriter(teeWriter)
-
-	defer bufwriter.Flush()
 
 	io.Copy(teeWriter, os.Stdin)
+	os.Exit(0)
 }
